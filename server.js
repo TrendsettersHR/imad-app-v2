5aa6ed333adee898c3cdc373189dc227ebb96633
TrendsettersHR <trendsette.hr@gmail.com>
2017-02-27 17:33:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -21,4 +23,9 @@ app.get('/ui/madi.png', function (req, res) {
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
+});
+var counter = 0
+app,get('/counter', function(req,res) {
+    counter = counter +1
+    res.send(counter.tostring())
 });
