579ddaa31af8e7d31efb150315fb8ace46d99da7
TrendsettersHR <trendsette.hr@gmail.com>
2017-03-02 17:02:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-
+//let me add this line so that it can read the next line..
 
 var element = document.getElementById("main-text");
 element.InnerHTML = "This value is inserted in the main.js";
