105ce4b7097241b32c6024f07f36dfb9ae79e88f
TrendsettersHR <trendsette.hr@gmail.com>
2017-03-03 15:19:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,11 @@
 console.log('Loaded!');
 //let me add this line so that it can read the next line..
 
-var element = document.getElementById("main-text");
+var element = document.getElementById("main-text"
+);
 alert("Executed Up to here");
 element.InnerHTML = "This value is inserted in the main.js";
-alert(element);
+alert("Executed after assigning the new value in element");
 
 var img = document.getElementById("croc");
 img.onclick = function ()
