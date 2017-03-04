af1bd08527cb7355b0bebc415ba5202c2837adfd
TrendsettersHR <trendsette.hr@gmail.com>
2017-03-04 22:25:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,2 @@
 alert("This is from main.js");
 
-var element=document.getElementById("main-text") ;
-element.innerHTML = "New Value";
- 
- 
- var button = document.getElementById("counter");
- var count = 0;
- button.onclick = function() {
-     count = count+1;
-     var span = document.getElementById("count");
-     span.innerHTML = counter;
-    
- };
