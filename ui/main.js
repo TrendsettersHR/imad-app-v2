fb3990f507a1bed3843c487d13b28dce5c7b0a87
TrendsettersHR <trendsette.hr@gmail.com>
2017-03-06 10:18:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
-alert("Hi am from main.js");
-//var element = document.getElementByID("main-text");
-//element.innerHTML = "New Value";
+alert("Hi am from main.js let us see if main-text changes to new value");
+var element = document.getElementByID("main-text");
+element.innerHTML = "New Value";
 //window.onload= function()
 //{
 //alert("Hi am from main.js");
 //var element = document.getElementByID("main-text");
 //element.innerHTML = "New Value";
 //};
+alert("Has it changed???")
 $('document').ready(function(){
       var element = document.getElementById('main-text');
       element.innerHTML = 'This value is inserted in the main.js';
