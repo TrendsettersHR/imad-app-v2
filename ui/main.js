aa9883459902ce210c1ea8af5e068a79af765e21
TrendsettersHR <trendsette.hr@gmail.com>
2017-03-06 10:11:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,18 @@
 //alert("Hi am from main.js");
 //var element = document.getElementByID("main-text");
 //element.innerHTML = "New Value";
-window.onload= function()
-{
-alert("Hi am from main.js");
-var element = document.getElementByID("main-text");
-element.innerHTML = "New Value";
-};
+//window.onload= function()
+//{
+//alert("Hi am from main.js");
+//var element = document.getElementByID("main-text");
+//element.innerHTML = "New Value";
+//};
+$('document').ready(function(){
+      var element = document.getElementById('main-text');
+      element.innerHTML = 'This value is inserted in the main.js';
+      var img = document.getElementById('croc');
+      img.onclick = function ()
+      {
+          img.style.marginleft = '100px';
+      };
+});
