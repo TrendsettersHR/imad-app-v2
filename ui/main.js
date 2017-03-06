404af7134a5ae75628a31fb72b6685642cb47f1c
TrendsettersHR <trendsette.hr@gmail.com>
2017-03-06 10:04:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
+//alert("Hi am from main.js");
+//var element = document.getElementByID("main-text");
+//element.innerHTML = "New Value";
+window.onload= function()
+{
 alert("Hi am from main.js");
 var element = document.getElementByID("main-text");
 element.innerHTML = "New Value";
+};
