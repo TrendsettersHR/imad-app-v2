9b0681e7d0d405ac137c9e3a2a4cf66a90529ada
TrendsettersHR <trendsette.hr@gmail.com>
2017-03-07 16:48:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ var imgObj = null;
                imgObj.style.left = parseInt(imgObj.style.left) + 100 + 'px';
             }
             
-            window.onload =init;
+            // window.onload =init;
  
  
   // document.getElementById('croc').onclick = function()
