23dadf23fcf29870d8fc85000d3ef79e7d08875d
TrendsettersHR <trendsette.hr@gmail.com>
2017-03-11 13:26:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,6 +27,8 @@ function moveRight(){
   
                imgObj.style.left = parseInt(imgObj.style.left) + 100 + 'px';
                counter = counter + 1;
+               var span = getElementById("cnt")
+               span.innerHTML = counter.toString(); 
                
                 
 }
